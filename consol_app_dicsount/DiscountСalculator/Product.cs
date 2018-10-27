11a0f0f92fc09beb503a8c43d85dee1dc2e0f1ef
DESKTOP-PIV6EBB\mi2 <vova.betelev@yandex.ru>
2018-10-27 12:38:29 +0300

    First commit. App for discount calculator.

---
+++ consol_app_dicsount/DiscountСalculator/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DiscountСalculator
+{
+    public class Product : IDiscount
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int typeDiscount { get; set; }
+        public int DiscountValue { get; set; }
+        public DateTime? StartSellDate { get; set; }
+        public DateTime? EndSellDate { get; set; }
+        public int priseCard { get; set; }
+        public DateTime? startCard { get; set; }
+        public DateTime? endCard { get; set; }
+        public int priseDelta { get; set; }
+        
+        public int CalculateDiscountPrice()
+        {
+            int pr = -1;
+            if(typeDiscount == 1)
+            {
+                if (startCard.HasValue && endCard.HasValue && startCard <= DateTime.UtcNow && endCard >= DateTime.UtcNow)
+                {
+                    pr = Price - priseCard;
+                    if (pr < 0)
+                    {
+                        pr = 0;
+                    }
+                }
+            } else if (typeDiscount == 2) {
+                if (DiscountValue != 0 && StartSellDate.HasValue && EndSellDate.HasValue && StartSellDate <= DateTime.UtcNow && EndSellDate >= DateTime.UtcNow)
+                {
+                    pr = Price - (Price * DiscountValue / 100);
+                }
+            } else if (typeDiscount == 3) {
+                pr = Price - priseDelta;
+            }
+            return pr;
+        }
+
+        public string GetSellInformation()
+        {
+        string answer = " ";
+        if (typeDiscount == 1) {
+            if (CalculateDiscountPrice() != -1 && startCard.HasValue && endCard.HasValue)
+            {
+                answer = $"Ваша подарочная карта активна." + $"Стоимость со скидкой - {CalculateDiscountPrice()} р.";
+            } else {
+                answer = $"Ваша подарочная карта не активна." + $"Стоимость - {Price} р.";
+            }
+        } else 
+        if (typeDiscount == 2) {
+            if (DiscountValue != 0 && StartSellDate.HasValue && EndSellDate.HasValue && StartSellDate <= DateTime.UtcNow && EndSellDate >= DateTime.UtcNow)
+            {
+                answer = $"На данный товар действует скидка {DiscountValue}% в период с {StartSellDate.Value.ToShortDateString()} по {EndSellDate.Value.ToShortDateString()}. " + $"Сумма с учётом скидки - {CalculateDiscountPrice()}р.";
+            } else {
+                answer = "В настоящий момент на данный товар нет скидок.";
+            }
+        } else
+        if (typeDiscount == 3) {
+            answer = $"Стоимость с учётом скидки - {CalculateDiscountPrice()} р.";
+        }
+            return answer;
+        }
+    }        
+}
---
+++ consol_app_dicsount/DiscountСalculator/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace DiscountСalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer_main = 1;
+            while (answer_main == 1) {
+                Console.WriteLine("Вы хотите добавить новый продукт? 1 - да, 2 - нет");
+
+                int.TryParse(Console.ReadLine(), out answer_main);
+                if (answer_main == 1)
+                {
+                    CreateProduct();
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void CreateProduct()
+        {
+            var product = new Product();
+
+            Console.WriteLine("Введите название продукта");
+
+            product.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите стоимость продукта");
+
+            int.TryParse(Console.ReadLine(), out var price);
+
+            while (price == 0)
+            {
+                Console.WriteLine("Стоимость продукта не была введена или она была введена с ошибкой. Пожалуйста, введите стоимость продукта ещё раз");
+
+                int.TryParse(Console.ReadLine(), out price);
+            }
+
+            product.Price = price;
+
+            Console.WriteLine("Введите тип скидки на товар : " + $"\n   1 - скидочная карта" + $"\n   2 - % от стоимости" + $"\n  3 - сумма от стоимости");
+
+            int.TryParse(Console.ReadLine(), out var discountType);
+
+            product.typeDiscount = discountType;
+
+            if (discountType == 1)
+            {
+                Console.WriteLine("Введите кол-во денег на карте : ");
+
+                int.TryParse(Console.ReadLine(), out var cardPrice);
+
+                while (cardPrice < 0)
+                {
+                    Console.WriteLine("Значение денег на карте было введено не верно. Оно не может быть отрицательным.");
+
+                    int.TryParse(Console.ReadLine(), out cardPrice);
+                }
+
+                product.priseCard = cardPrice;
+
+                Console.WriteLine("Введите дату начала действия карты");
+
+                DateTime.TryParse(Console.ReadLine(), out var cardStart);
+
+                if (cardStart != DateTime.MinValue)
+                {
+                    product.startCard = cardStart;
+                }
+
+                Console.WriteLine("Введите дату окончания действия карты");
+
+                DateTime.TryParse(Console.ReadLine(), out var cardEnd);
+
+                if (cardEnd != DateTime.MinValue)
+                {
+                    product.endCard = cardEnd;
+                }
+            }
+            else if (discountType == 2)
+            {
+                Console.WriteLine("Введите значение скидки на товар (в % от общей стоимости)");
+
+                int.TryParse(Console.ReadLine(), out var discountValue);
+
+                while (discountValue > 100)
+                {
+                    Console.WriteLine("Значение скидки не может быть больше 100");
+
+                    int.TryParse(Console.ReadLine(), out discountValue);
+                }
+
+                product.DiscountValue = discountValue;
+
+                Console.WriteLine("Введите дату начала действия скидки");
+
+                DateTime.TryParse(Console.ReadLine(), out var startSellDate);
+
+                if (startSellDate != DateTime.MinValue)
+                {
+                    product.StartSellDate = startSellDate;
+                }
+
+                Console.WriteLine("Введите дату окончания действия скидки");
+
+                DateTime.TryParse(Console.ReadLine(), out var endSellDate);
+
+                if (endSellDate != DateTime.MinValue)
+                {
+                    product.EndSellDate = endSellDate;
+                }
+            } else if (discountType == 3) {
+                Console.WriteLine("Введите цену, которой равна скидка : ");
+
+                int.TryParse(Console.ReadLine(), out var deltaPrice);
+
+                while (deltaPrice > price)
+                {
+                    Console.WriteLine("Скидка была введена с ошибкой. Значение не может быть больше цены товара.");
+
+                    int.TryParse(Console.ReadLine(), out deltaPrice);
+                }
+
+                product.priseDelta = deltaPrice;
+            }
+            Console.WriteLine($"Вы успешно добавили новый продукт: {product.Name}, стоимость - {product.Price}р. {product.GetSellInformation()}");
+        }
+    }
+}
